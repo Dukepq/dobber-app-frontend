4406d19f596cfb18fc2bb315e7d6b3bfbe8c92b3
Dukepq <maikelvdj@live.be>
2023-05-19 01:44:32 +0200

    minor adjustment

--- src/extremesDef.js
+++ src/extremesDef.js
@@ -3,7 +3,7 @@
     depthRatioA: 4,
     depthRatioB: 1/4,
     spread: 0.02,
-    recentVolume: 5000,
+    recentVolume: 10000,
     PoP: null,
     volumeTimesSpread: null,
     volatilityIndex: 2,
